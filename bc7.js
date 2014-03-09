5f300af97e92c5ed3d73f1cea43d387c92c4acb6
Crazy4Redstone <mcpe1398@gmail.com>
2014-03-09 03:03:56 -0500

    Update bc7.js

--- bc7.js
+++ bc7.js
@@ -5,12 +5,12 @@
 // Blocks:
 
 // Mining Well
-Block.defineBlock (174,'Mining Well',[['furnace_bottom',0],['stonecutter_top',0],['furnace_side',0],['furnace_side',0],['furnace_side',0],['furnace_side',0]],50,false,0)
+Block.defineBlock (174,'Mining Well',[['furnace',3],['stonecutter',2],['furnace',2],['furnace',2],['furnace',2],['furnace',2]],50,false,0)
 Block.setShape (174,0,0,0,1,1,1)
 Block.setDestroyTime (174,0.25)
 
 // Auto Crafting Table
-Block.defineBlock (175,'Automatic Crafting Table',[['oak_plank',0],['stonecutter_top',0],['crafting_table_side',0],['crafting_table_side',0],['crafting_table_side',0],['crafting_table_side',0]],50,false,0)
+Block.defineBlock (175,'Automatic Crafting Table',[['planks',0],['stonecutter',2],['crafting_table',1],['crafting_table',1],['crafting_table',1],['crafting_table',1]],50,false,0)
 Block.setShape (175,0,0,0,1,1,1)
 Block.setDestroyTime (175,0.25)
 
@@ -25,11 +25,11 @@ Block.setShape (177,0,0,0,1,1,1)
 Block.setDestroyTime (177,0.25)
 
 // Wood Pipe
-Block.defineBlock (178,'Wood Pipe',[['oak_plank',0]],5,true,11)
+Block.defineBlock (178,'Wood Pipe',[['planks',0]],50,true,11)
 Block.setDestroyTime (178,0.25)
 
 // Cobblestone Pipe
-Block.defineBlock (179,'Cobblestone Pipe',[['cobblestone',0]],4,true,11)
+Block.defineBlock (179,'Cobblestone Pipe',[['cobblestone',0]],50,true,11)
 Block.setDestroyTime (179,0.25)
 
 // Commands:
