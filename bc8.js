b7cea2b3d19595492136da507fcd3892eed47e86
Crazy4Redstone <mcpe1398@gmail.com>
2014-03-09 12:26:41 -0500

    Update bc8.js

--- bc8.js
+++ bc8.js
@@ -16,17 +16,17 @@ ModPE.setItem (450,'name_tag',0,'Wrench')
 // Blocks:
 
 // Mining Well
-Block.defineBlock (174,'Mining Well',[['furnace',3],['stonecutter',2],['furnace',2],['furnace',2],['furnace',2],['furnace',2]],50,false,0)
+Block.defineBlock (174,'Mining Well',[['furnace',3],['stonecutter',2],['furnace',2],['furnace',2],['furnace',2],['furnace',2]],61,false,0)
 Block.setShape (174,0,0,0,1,1,1)
 Block.setDestroyTime (174,0.25)
 
 // Auto Crafting Table
-Block.defineBlock (175,'Automatic Crafting Table',[['planks',0],['stonecutter',2],['crafting_table',1],['crafting_table',1],['crafting_table',1],['crafting_table',1]],50,false,0)
+Block.defineBlock (175,'Automatic Crafting Table',[['planks',0],['stonecutter',2],['crafting_table',1],['crafting_table',1],['crafting_table',1],['crafting_table',1]],58,false,0)
 Block.setShape (175,0,0,0,1,1,1)
 Block.setDestroyTime (175,0.25)
 
 // Builder
-Block.defineBlock (176,'Builder',[['stonecutter',3],['stonecutter',3],['stonecutter',0],['stonecutter',0],['stonecutter',0],['stonecutter',0]],50,false,0)
+Block.defineBlock (176,'Builder',[['stonecutter',3],['stonecutter',3],['stonecutter',0],['stonecutter',0],['stonecutter',0],['stonecutter',0]],61,false,0)
 Block.setShape (176,0,0,0,1,1,1)
 Block.setDestroyTime (176,0.25)
 
@@ -36,15 +36,15 @@ Block.setShape (177,0,0,0,1,1,1)
 Block.setDestroyTime (177,0.25)
 
 // Wood Pipe
-Block.defineBlock (178,'Wood Pipe',[['planks',0]],50,true,11)
+Block.defineBlock (178,'Wood Pipe',[['planks',0]],58,true,11)
 Block.setDestroyTime (178,0.25)
 
 // Cobblestone Pipe
-Block.defineBlock (179,'Cobblestone Pipe',[['cobblestone',0]],50,true,11)
+Block.defineBlock (179,'Cobblestone Pipe',[['cobblestone',0]],61,true,11)
 Block.setDestroyTime (179,0.25)
 
 // Mining Well Pipe
-Block.defineBlock (180,'Mining Well Pipe',[['wool',14]],50,false,11)
+Block.defineBlock (180,'Mining Well Pipe',[['wool',14]],58,false,11)
 Block.setDestroyTime (180,0.25)
 
 // Functions:
